1b1fc4b62831cb12e75eb129a454bfb2e4eff9e5
skrall3119 <alexjanci@yahoo.com>
2021-10-26 23:51:56 -0500

    init

--- TableViewVisualApp/ViewController.swift
+++ TableViewVisualApp/ViewController.swift
@@ -7,11 +7,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let petArray = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical fish", "goldfish", "sea horses", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "cellID")
+        cell?.textLabel?.text = petArray[indexPath.row]
+        return cell!
     }
 
 
