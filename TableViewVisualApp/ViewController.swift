7a342521c02f3eea669ba36e99c83efa7555d097
skrall3119 <alexjanci@yahoo.com>
2021-10-26 23:37:58 -0500

    Initial Commit

---
+++ TableViewVisualApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewVisualApp
+//
+//  Created by Alex Janci on 10/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
